753da8f24b98a0c2768e791ee3cd1e5267e78b4b
NADJIB <nadjibboudali@gmail.com>
2024-04-22 10:46:05 +0100

    i2

--- src/test/java/exercice3/ProductServiceTest.java
+++ src/test/java/exercice3/ProductServiceTest.java
@@ -18,14 +18,14 @@ class ProductServiceTest {
         ProductService productService = new ProductService(productApiClient);
 
         // Définir le comportement du mock pour simuler le retour d'un produit
-        Product product = new Product("23", "Armani Aqua di Gio", 600.0);
-        Mockito.when(productApiClient.getProduct("23")).thenReturn(product);
+        Product product = new Product("87", "ecole militaire", 600.0);
+        Mockito.when(productApiClient.getProduct("87")).thenReturn(product);
 
         // Appeler la méthode getProduct de ProductService
-        Product ResultProduct = productService.getProduct("23");
+        Product ResultProduct = productService.getProduct("87");
 
         // Vérifier que la méthode getProduct du mock ProductApiClient a été appelée avec le bon argument
-        Mockito.verify(productApiClient).getProduct("23");
+        Mockito.verify(productApiClient).getProduct("87");
 
         // Vérifier que le produit retourné par ProductService est le même que celui retourné par le mock ProductApiClient
         assertEquals(product, ResultProduct);
