f87dc9200390e767e62667695929057898185b18
Nadjibboudali <158842137+Nadjibboudali@users.noreply.github.com>
2024-04-22 14:57:05 +0100

    Update UserServiceTest.java

--- src/test/java/exercice01/UserServiceTest.java
+++ src/test/java/exercice01/UserServiceTest.java
@@ -8,7 +8,7 @@ class UserServiceTest {
 
     @org.junit.jupiter.api.Test
     void getUserById() {
-        // Créer un mock pour l'interface UserRepository
+        
         UserRepository userRepositoryMock = mock(UserRepository.class);
 
         // Créer un utilisateur
@@ -17,16 +17,13 @@ class UserServiceTest {
         // Définir le comportement du mock pour la méthode findUserById
         when(userRepositoryMock.findUserById(1)).thenReturn(user);
 
-        // Créer une instance de la classe UserService en lui passant le mock
         UserService userService = new UserService(userRepositoryMock);
 
-        // Appeler la méthode getUserById avec l'ID de l'utilisateur
         User result = userService.getUserById(1);
 
-        // Vérifier que la méthode findUserById sur l'objet mock est appelée avec le bon argument
         verify(userRepositoryMock).findUserById(1);
 
-        // Vérifier que le résultat correspond à l'utilisateur factice
+        
         assertEquals(user, result);
     }
 }
